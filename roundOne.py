d35abd834b8b2ebc0ee03dfaebb5095c949c75d8
Gopal Narayanan <gopalnarayanantn@gmail.com>
2021-01-11 09:27:49 +0530

    Patter printing question in python

--- roundOne.py
+++ roundOne.py
@@ -1,1 +1,12 @@
 # Round - 1
+def fn(n):
+    for x in range(n+1):
+        for y in range(n):
+            if y >= x:
+                print("{}".format(n-x), end='')
+        print('\n')
+    return "\n"
+
+
+n = int(input("Enter a number\n").strip())
+print(fn(n))
